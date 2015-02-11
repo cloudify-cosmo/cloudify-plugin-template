7af9592d1eb27840ae1d7bed217bf533a658b32c
ran <ran@gigaspaces.com>
2015-02-11 16:06:26 +0200

    modified plugins-common and dsl-parser dependency versions to >=3.1

--- setup.py
+++ setup.py
@@ -35,10 +35,10 @@ setup(
     zip_safe=False,
     install_requires=[
         # Necessary dependency for developing plugins, do not remove!
-        "cloudify-plugins-common==3.1"
+        "cloudify-plugins-common>=3.1"
     ],
     test_requires=[
-        "cloudify-dsl-parser==3.1"
+        "cloudify-dsl-parser>=3.1"
         "nose"
     ]
 )
