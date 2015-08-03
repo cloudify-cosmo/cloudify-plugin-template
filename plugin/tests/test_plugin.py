d633122826163eefaf120b9cfe5ef4a44ad09845
maxim <maxim@gigaspaces.com>
2015-08-03 16:20:32 +0300

    CFY-3009 Switching to local workflow decorator.

--- plugin/tests/test_plugin.py
+++ plugin/tests/test_plugin.py
@@ -14,41 +14,33 @@
 #    * limitations under the License.
 
 
-import os
+from os import path
 import unittest
 
-from cloudify.workflows import local
+from cloudify.test_utils import workflow_test
 
 
 class TestPlugin(unittest.TestCase):
 
-    def setUp(self):
-        # build blueprint path
-        blueprint_path = os.path.join(os.path.dirname(__file__),
-                                      'blueprint', 'blueprint.yaml')
-
-        # inject input from test
-        inputs = {
-            'test_input': 'new_test_input'
-        }
-
-        # setup local workflow execution environment
-        self.env = local.init_env(blueprint_path,
-                                  name=self._testMethodName,
-                                  inputs=inputs)
-
-    def test_my_task(self):
-
+    @workflow_test(path.join('blueprint', 'blueprint.yaml'),
+                   resources_to_copy=[path.join('blueprint',
+                                                'test_plugin.yaml')],
+                   inputs={'test_input': 'new_test_input'})
+    def test_my_task(self, cfy_local):
         # execute install workflow
-        self.env.execute('install', task_retries=0)
+        """
+
+        :param cfy_local:
+        """
+        cfy_local.execute('install', task_retries=0)
 
         # extract single node instance
-        instance = self.env.storage.get_node_instances()[0]
+        instance = cfy_local.storage.get_node_instances()[0]
 
         # assert runtime properties is properly set in node instance
         self.assertEqual(instance.runtime_properties['value_of_some_property'],
                          'new_test_input')
 
         # assert deployment outputs are ok
-        self.assertDictEqual(self.env.outputs(),
+        self.assertDictEqual(cfy_local.outputs(),
                              {'test_output': 'new_test_input'})
