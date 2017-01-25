42a4dce5010bdfaddadfaebc3d6462944c58afc2
opencm <limor@gigaspaces.com>
2017-01-25 15:32:48 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -35,10 +35,10 @@ setup(
     zip_safe=False,
     install_requires=[
         # Necessary dependency for developing plugins, do not remove!
-        "cloudify-plugins-common>=4.0a12"
+        "cloudify-plugins-common>=4.0a13"
     ],
     test_requires=[
-        "cloudify-dsl-parser>=4.0a12"
+        "cloudify-dsl-parser>=4.0a13"
         "nose"
     ]
 )
