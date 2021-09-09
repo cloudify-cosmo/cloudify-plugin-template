abd05997d90e425ebdf3ead0ba40d203e3d669c0
opencm <limor@gigaspaces.com>
2021-09-09 14:27:48 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -35,10 +35,10 @@ setup(
     zip_safe=False,
     install_requires=[
         # Necessary dependency for developing plugins, do not remove!
-        "cloudify-common>=5.2.5"
+        "cloudify-common>=5.2.6.dev1"
     ],
     test_requires=[
-        "cloudify-common>=5.2.5"
+        "cloudify-common>=5.2.6.dev1"
         "nose"
     ]
 )
